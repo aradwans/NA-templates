90c0263bf4e7aae6d19c356b1d56aa823d051766
Agnieszka Radwanska <agnieszka.radwanska85@gmail.com>
2016-11-10 13:53:30 +0100

    add template specialization for boost::optional

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstddef>
 #include <utility>
+#include <boost/optional.hpp>
 template<typename T>
 class container_wrapper{
 public:
@@ -20,6 +21,33 @@ public:
 private:
 
 T _value;
+
+};
+template<>
+std::size_t container_wrapper<int>::size() const{
+return 1;
+}
+
+template<typename T>
+class container_wrapper<boost::optional<T>>{
+public:
+	container_wrapper()=default;
+	container_wrapper(boost::optional<T> arg): _value(std::move(arg))
+	{
+	}
+	container_wrapper(const container_wrapper &) = default;
+	container_wrapper(container_wrapper &&) = default;
+	container_wrapper & operator=(const container_wrapper &) = default;
+	container_wrapper & operator=( container_wrapper &&) = default;
+	std::size_t size() const 
+	{
+	return _value ? 1 : 0;
+	
+	}
+
+private:
+
+boost::optional<T> _value;
 
 };
 
